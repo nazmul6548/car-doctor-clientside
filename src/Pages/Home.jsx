ce2088e07c18760356697f204c2f14e64643588c
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-05-02 10:02:13 +0600

    task

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
+import Slider from "../component/Slider";
 
 
 const Home = () => {
     return (
         <div className="min-h-screen">
-            <h1>this is home</h1>
+            <Slider></Slider>
         </div>
     );
 };
